b6681b0e5cffc8ca402167b79c589f79b1cf4216
cary-cyon <kozvlad99@gmail.com>
2022-06-18 15:03:17 +0300

    InitalCreate

---
+++ INFOION/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace INFOION.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
